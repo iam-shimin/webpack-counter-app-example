81a12441388dea3853497e858a38b52f7f616b0e
shimin mathew <simplyshimin@gmail.com>
2020-09-24 21:57:27 +0530

    setup html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
 const path = require('path');
 
-// caching
-// hot-module-replacement
-// lazy-loading
-// prefetching
-// preloading 
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+// * HTML-webpack-plugin
+// o minification
+// o lazy-loading
+// o caching
+// o prefetching
+// o preloading 
+// o hot-module-replacement
 
 module.exports = {
 	mode: 'development',
 	entry: path.resolve(__dirname, 'src', 'app.js'),
-	output: {
-		publicPath: '/dist'
-	},
+	// output: {
+	// 	publicPath: '/dist'
+	// },
 	resolve: {
 		modules: ['src', path.resolve(__dirname, 'src', 'js'), 'node_modules']
 	},
@@ -19,5 +24,17 @@ module.exports = {
 		rules: [
 			{test: /\.css$/, use: ['style-loader', 'css-loader']}
 		]
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'index.html',
+			title: 'Counter | Create counters ...',
+			scriptLoading: 'defer',
+			showErrors: true
+			// favicon,
+			// meta,
+			// cache,
+			// hash,
+		})
+	]
 }
